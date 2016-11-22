91a2505990274faeb05be8b06d262144b109a070
Cali <calinoracation@gmail.com>
2016-11-22 09:14:35 -0800

    Update renew commands

--- renew_certs.sh
+++ renew_certs.sh
@@ -3,4 +3,4 @@
 echo "deb http://httpredir.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/sources.list
 apt-get update
 apt-get -qq install certbot -t jessie-backports
-certbot certonly --webroot -w /usr/share/nginx/html -d calinoracation.com -d www.calinoracation.com
+certbot renew
