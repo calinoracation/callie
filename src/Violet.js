64a1d4eddd9f9b58972be8a354c711866094a4e6
Callie Riggins <calinoracation@gmail.com>
2017-05-06 21:38:34 -0700

    Overhaul park, re-enable callie

--- src/Violet.js
+++ src/Violet.js
@@ -1,114 +1,331 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled, { css, keyframes } from 'styled-components';
+import { compose, withState } from 'recompose';
+import { Helmet } from 'react-helmet';
 
-import './Violet.css';
+// const apiHost = process.env.NODE_ENV === 'production' ?
+//   'http://violetfunparkparty.com:5000' :
+//   'http://localhost:5000';
 
-const apiHost = process.env.NODE_ENV === 'production' ?
-  'http://violetfunparkparty.com:5000' :
-  'http://localhost:5000';
+// function handleSubmit(event, name, email, phone){
+//   evt.preventDefault();
 
-class Violet extends Component {
-  state = {};
+//   console.log(`${name} has RSVP'd. Send an email to ${email} or call @ ${phone}.`);
 
-  handleDismiss(){
-    this.setState({disabled: false});
-  }
+//   return fetch(`${apiHost}/rsvp`, {
+//     method: `post`,
+//     mode: `cors`,
+//     headers: {
+//       'Content-Type': `application/json`,
+//       'Accept': `application/json`,
+//     },
+//     body: JSON.stringify({name, email, phone}),
+//   });
+// }
 
-  handleSubmit(evt){
-    evt.preventDefault();
+const enhance = compose(
+  withState('disabled', 'setDisabled', false),
+  withState('hasSent', 'setHasSent', false),
+);
 
-    const name = this.name_.value;
-    const email = this.email_.value;
-    const phone = this.phone_.value;
-
-    console.log(`${name} has RSVP'd. Send an email to ${email} or call @ ${phone}.`);
-
-    fetch(`${apiHost}/rsvp`, {
-      method: `post`,
-      mode: `cors`,
-      headers: {
-        'Content-Type': `application/json`,
-        'Accept': `application/json`,
-      },
-      body: JSON.stringify({name, email, phone}),
-    })
-    .then(() => {
-      this.setState({
-        hasSent: true,
-        disabled: true,
-      });
-    });
-  }
-
-  render() {
-    return (
-      <div style={{position: 'relative'}}>
-        <div className={`Violet ${this.state.disabled && 'disabled'}`}>
-          <header>
-            <h1>Violet's Fun-Raising Neighborhood Party</h1>
-            <div className="header--wrapper">
-              <span className="header--benefit">A fund raiser for the Girl Scouts &amp; Tree Frog Treks</span>
-              <div className="header--details">
-                <h3 className="header--location">At Cayuga Playground on June 11<sup>th</sup></h3>
-                <h3 className="header--time">11:00<sup>am</sup> - 1<sup>pm</sup></h3>
-              </div>
-            </div>
-          </header>
-          <main>
-            <div className="main--description">
-              <h2>
-                Come meet your neighbors &amp; have fun while raising funds for your local Girl Scouts &amp; Tree Frog Treks to send kids to summer camp!
-
-                <br /><br />Due to the limited space at the park, please RSVP so that we can let the park know how many people will be attending. We also want to cater enough food and drinks for everyone.
-              </h2>
-              <form className="rsvp" onSubmit={this.handleSubmit.bind(this)}>
-                <label>
-                  <span>Name</span>
-                  <input ref={name => this.name_ = name} type="text" id="name" autoComplete="name" name="name" />
-                </label>
-                <label>
-                  <span>Email</span>
-                  <input ref={email => this.email_ = email} type="email" id="email" autoComplete="email" name="email" />
-                </label>
-                <label>
-                  <span>Phone</span>
-                  <input ref={phone => this.phone_ = phone} type="tel" id="phone" placeholder="(optional)" name="phone" autoComplete="tel" />
-                </label>
-                <input type="submit" value="Click here to RSVP" />
-              </form>
-            </div>
-            <ul className="main--photos">
-              <li>
-                <img src="/face-painting.jpg" alt="Face Painting" />
-                <span className="main--photo-description">Free face painting!</span>
-              </li>
-              <li>
-                <img src="/yoga.jpg" alt="Yoga Pose" />
-                <span className="main--photo-description">Free yoga class!</span>
-              </li>
-              <li>
-                <img src="/party.jpg" alt="Community Building" />
-                <span className="main--photo-description">Community Building</span>
-              </li>
-            </ul>
-          </main>
-          <footer>
-            <img src="/logo.png" role="presentation" />
-            <div className="footer--details">
-              <h4>MARKER  REAL  ESTATE</h4>
-              <span>425 Market  Street  Suite 2200, San Francisco, CA</span>
-              <span>415.713.9420 | violet@markersf.com | www.markersf.com</span>
-            </div>
-          </footer>
-        </div>
-        <div className={`thanks-container ${this.state.disabled && 'visible'}`}>
-          <div className="thanks">
-            <h2>Thank you for RSVP'ing, see you on June 11th! <br />Please check your email for any changes or updates</h2>
-            <button className="btn--dismiss" onClick={this.handleDismiss.bind(this)}>Close</button>
-          </div>
-        </div>
+const Violet = enhance(({ disabled, setDisabled }) => {
+  return (
+    <PageWrapper>
+      <Helmet>
+          <title>Violet's Fun-Raising Neighborhood Party</title>
+      </Helmet>      
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <Logo>
+          <LeftBirds high />   
+          <LeftBirds />
+          <img src="/logo--park-party.png" alt="Party in the Park" />          
+          <RightBirds />   
+          <RightBirds high />   
+        </Logo>
+        <ContentDescription 
+          size={30}
+          margin="1vw 0 0 0">
+          Meet your neighbors while benefiting
+        </ContentDescription>
+        <Benefits>
+          <img src="/benefit--tree-frog-treks.png" alt="Benefitting Tree Frog Treks" />
+        </Benefits>
+        <ContentBox>
+          <ContentHeader>Violet's Fun Raiser</ContentHeader>
+          <ContentDate>June 11th @ 11am</ContentDate>
+          <ContentDescription>
+            Cayuga Playground, Outer Mission<br />
+            301 Naglee Ave, San Francisco 94112
+          </ContentDescription>
+          <RsvpWrapper>
+            <input type="text" placeholder="What's your email?" />
+            <input type="submit" value="RSVP" />
+          </RsvpWrapper>
+        </ContentBox>
+        <FunFeatures>
+          <FunFeature color="#f04c23">Free Yoga Class</FunFeature>
+          <FunFeature color="#3b55a5">Free Salsa Class</FunFeature>
+          <FunFeature color="#61c5b8">Free Bouncy House</FunFeature>
+          <FunFeature color="#f04c23">Face Painting</FunFeature>
+          <FunFeature color="#3b55a5">Food & Drinks</FunFeature>
+        </FunFeatures>
       </div>
-    );
+      <Thanks>
+        <div className="thanks">
+          <h2>Thank you for RSVP'ing, see you on June 11th! <br />Please check your email for any changes or updates</h2>
+          <button className="btn--dismiss" onClick={() => setDisabled(true)}>Close</button>
+        </div>
+      </Thanks>
+    </PageWrapper>
+  );
+});
+
+const media = {
+  bravo: (...args) => css`
+    @media (max-width: 540px) {
+      ${ css(...args) }
+    }
+  `
+};
+
+const FlyBirdyFlyLow = keyframes`
+  to {
+    opacity: 0;
+    transform: translate(-220px, 120px) scale(0.5, -0.2);
   }
-}
+`;
+
+const FlyBirdyFlyHigh = keyframes`
+  to {
+    opacity: 0;
+    transform: translate(-220px, -20px) scale(0.5, -0.5);
+  }
+`;
+
+const FlyBirdyFlyRight = keyframes`
+  to {
+    opacity: 0;
+    transform: translate(190px, 10px) scale(-1, -0.5);
+  }
+`;
+
+const FlyBirdyFlyRightHigh = keyframes`
+  to {
+    opacity: 0;
+    transform: translate(190px, -120px) scale(-1, -0.5);
+  }
+`;
+
+const Thanks = styled.div`
+  display: none;
+`;
+
+const PageWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: #B3E5FC url(/bg--park-and-sky.png) no-repeat center center fixed;
+  background-size: cover;
+  overflow: auto;
+  padding-bottom: 200px;
+`;
+
+const Logo = styled.header`
+  display: flex;
+  justify-content: center;
+  margin-top: 5vh;
+  position: relative;
+
+  img {
+    height: 25vh;
+    z-index: 10;
+  }
+`;
+
+const ContentBox = styled.section`
+  background: linear-gradient(-9deg, #f7ec1e 0,#ed1f24 100%);
+  box-shadow: 5px 5px 27px 3px rgba(0,0,0,0.75);
+  display: flex;
+  flex-direction: column;
+  transform: rotate(-1deg);
+  border-radius: 5px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 10px 50px;
+  margin: 20px 5vw;
+  min-height: 20vh;
+
+  ${ media.bravo`
+    margin: 20px 0;
+    padding: 10px 10px;
+  ` }
+`;
+
+const LeftBirds = styled.div`
+  width: 350px;
+  min-height: 150px;
+  background-image: url(/left-birds.png);
+  background-repeat: no-repeat;
+  background-size: contain;
+  z-index: 1;
+  position: absolute;
+  transform: translate(90px, 20px) ${props => props.high ? 'scale(1)' : 'scale(0.8)'};
+  animation: ${props => props.high ? FlyBirdyFlyHigh : FlyBirdyFlyLow} 4s cubic-bezier(0.420, 0.000, 1.000, 1.000) infinite;
+  animation-delay: ${props => props.high ? 0 : 2.3}s;
+`;
+
+const RightBirds = styled.div`
+  width: 350px;
+  min-height: 150px;
+  background-image: url(/left-birds.png);
+  background-repeat: no-repeat;
+  background-size: contain;
+  z-index: 1;
+  position: absolute;
+  transform: translate(-90px, 10px) ${props => props.high ? 'scaleX(-1)' : 'scaleX(-1)'};
+  animation: ${props => props.high ? FlyBirdyFlyRightHigh : FlyBirdyFlyRight} 4s cubic-bezier(0.420, 0.000, 1.000, 1.000) infinite;
+  animation-delay: ${props => props.high ? 0 : 2}s;
+`;
+
+const FunFeature = styled.li`
+  color: ${props => props.color || '#fff'};
+  text-shadow:
+     -2px -2px 0 #fff,  
+      2px -2px 0 #fff,
+      -2px 2px 0 #fff,
+       2px 2px 0 #fff;
+
+  ${ media.bravo`
+    white-space: nowrap;
+    text-align: center;
+  ` }
+`;
+
+const FunFeatures = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  text-transform: uppercase;
+  font-family: 'komika_axisregular';
+  font-size: 22px;  
+  margin: 10px 0 0 0;
+  display: flex;
+  width: 550px;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  li {
+    width: 50%;
+    text-align: center;
+
+    ${ media.bravo`
+      width: auto;
+    ` }
+  }
+
+  ${ media.bravo`
+    flex-wrap: nowrap;
+    flex-direction: column;
+    width: auto;
+  ` }
+`;
+
+const ContentHeader = styled.h1`
+  color: #fff;
+  text-transform: uppercase;
+  text-shadow: 3px 3px 0 #cd371e;
+  font-family: 'komika_axisregular';
+  font-size: 35px;
+  font-weight: normal;
+  margin: 0;
+
+  ${ media.bravo`
+    font-size: 5vw;
+  ` }
+`;
+
+const ContentDate = styled.h2`
+  color: #489643;
+  text-transform: uppercase;
+  text-shadow: 3px 3px 0 #fff;
+  font-family: 'komika_axisregular';
+  font-size: 35px;
+  font-weight: normal;
+  margin: -10px 0 0 0;
+
+  ${ media.bravo`
+    font-size: 4vw;
+    text-shadow: 1.5px 1.5px 0 rgba(255, 255, 255, 0.8);
+    margin-top: 0;
+  ` }
+`;
+
+const ContentDescription = styled.h2`
+  color: #fae714;
+  text-transform: uppercase;
+  text-shadow: 2px 2px 2px #000;
+  font-family: 'komika_axisregular';
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  font-size: ${props => props.size || '17px'};
+  font-weight: normal;
+  margin: ${props => props.margin || '1vh 0 3vh 0'};
+
+  ${ media.bravo`
+    font-size: 3vw;
+  ` }
+`;
+
+const RsvpWrapper = styled.div`
+  margin: 0 0 30px 0;
+  display: flex;  
+  position: relative;  
+
+  input[type="submit"] {
+    display: flex;
+    background: #f26222;
+    color: #fff;
+    font-family: 'komika_axisregular';
+    font-size: 13px;
+    font-weight: 100px;
+    border: 3px solid #fff;
+    border-radius: 0 4px 4px 0;
+    border-width: 1px 1px 1px 0;    
+    margin: 0 0 0 -5px;
+  }
+
+  input[type="text"] {
+    font-family: 'komika_axisregular';
+    font-size: 13px;
+    border: 3px solid #fff;
+    border-radius: 4px;
+    color: #3c3c3c;
+    outline: none;
+    padding: 0 2px;
+
+    ${ media.bravo`
+      font-size: 10px;
+    ` }
+  }
+`;
+
+const Benefits = styled.h3`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: -10px 0 -25px 0;
+
+  img {
+    width: 250px;
+  }
+`;
 
 export default Violet;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
-import Violet from './Violet';
+
 import './index.css';
 
-if (true){
+if (location.hostname === 'violetfunparkparty.com'){
+	const Violet = require('./Violet').default;
+
     ReactDOM.render(
       <Violet />,
       document.getElementById('root')
     );
 } else {
-    // ReactDOM.render(
-    //   <App />,
-    //   document.getElementById('root')
-    // );
+	const App = require('./App').default;
+    ReactDOM.render(
+      <App />,
+      document.getElementById('root')
+    );
 }
